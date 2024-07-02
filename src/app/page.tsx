969b9a1c0f2f13ed2eeb30484f9b7a8246612587
SaagarRaj <saagartiwari1992@gmail.com>
2024-07-02 12:26:12 -0700

    main page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,24 @@ import { useEffect } from "react";
 export default function Home() {
   const { arrayToSort, isSorting } = useSortingAlgorithmContext();
 
-  useEffect(() => {
-    console.log(arrayToSort);
-    console.log(isSorting);
-  })
+
   return (
-    <div>
-      Hello !!
-    </div>
+    <main className="font-montserrat absolute top-0 h-screen  inset-0 -z-10  w-full items-center [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)] ">
+      <div className="flex h-full justify-center">
+        <div id="content-container" className="flex max-w-[1020px] w-full flex-col lg:px-0 px-4">
+          <div className=" h-[66px] relative flex items-center justify-between w-full">
+            <h1 className=" text-gray-200 text-2xl font-light hidden md:flex ">Sorting Visualizer</h1>
+            <div>Controls</div>
+          </div>
+          <div className="relative h-[calc(100vh-66px)] w-full">
+            <div className="absolute bottom-[32px] w-full mx-auto left-0 right-0 flex justify-center items-end">
+              {arrayToSort.map((value, index) => (
+                <div key={index} className="array-line relative w-1 mx-0.5 shadow-lg opacity-70 rounded-lg default-line-color" style={{ height: `${value}px` }}></div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </main >
   );
 }
