cd9d91da972296251add2b894c61fcd460dae4b6
SaagarRaj <saagartiwari1992@gmail.com>
2024-07-02 11:48:37 -0700

    Context - Visualizer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
-import Image from "next/image";
+'use client';
+
+import { useSortingAlgorithmContext } from "@/context/Visualizer";
+import { useEffect } from "react";
+
 
 export default function Home() {
+  const { arrayToSort, isSorting } = useSortingAlgorithmContext();
+
+  useEffect(() => {
+    console.log(arrayToSort);
+    console.log(isSorting);
+  })
   return (
     <div>
       Hello !!
