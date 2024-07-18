b0122b911d89349969ab52fdbcf5e0763b5380ff
SaagarRaj <saagartiwari1992@gmail.com>
2024-07-17 20:01:25 -0700

    Description component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Select from "@/components/Input/Select";
 import { Slider } from "@/components/Input/Slider";
 import { useSortingAlgorithmContext } from "@/context/Visualizer";
 import { SortingAlgorithmType } from "@/shared/types";
-import { algorithmOptions, generateAnimationArray } from "@/shared/utils";
+import { algorithmOptions, generateAnimationArray, sortingAlgorithmsData } from "@/shared/utils";
 import { useEffect } from "react";
 import { FaPlayCircle } from "react-icons/fa";
 import { RxReset } from "react-icons/rx";
@@ -60,6 +60,44 @@ export default function Home() {
                 {requireReset ? (<RxReset className="text-gray-400 h-8 w-8 " />) : (<FaPlayCircle className="text-system-green60 h-8 w-8" />)}
               </button>
             </div>
+
+            <div className="hidden sm:flex absolute top-[120%] left-0 w-full">
+              <div className="flex w-full text-white p-4 rounded border border-gray-500 bg-black bg-opacity-10 gap-6">
+                <div className="flex flex-col items-start justify-start w-3/4 ">
+                  <h3 className="text-lg">{sortingAlgorithmsData[selectedAlgorithm].title}</h3>
+                  <p className="text-sm text-gray-400 pt-2">{sortingAlgorithmsData[selectedAlgorithm].description}</p>
+
+                </div>
+
+                <div className="flex flex-col items-start justify-start w-1/4 ">
+                  <h3 className="text-lg">Time Complexity</h3>
+                  <div className="flex flex-col gap-2">
+                    <p className="flex w-full text-sm text-gray-500">
+                      <span className="w-28">Worst Case:</span>
+                      <span>
+                        {sortingAlgorithmsData[selectedAlgorithm].worstCase}
+                      </span>
+                    </p>
+
+                    <p className="flex w-full text-sm text-gray-500">
+                      <span className="w-28">Average Case:</span>
+                      <span>
+                        {sortingAlgorithmsData[selectedAlgorithm].averageCase}
+                      </span>
+                    </p>
+
+                    <p className="flex w-full text-sm text-gray-500">
+                      <span className="w-28">Best Case:</span>
+                      <span>
+                        {sortingAlgorithmsData[selectedAlgorithm].bestCase}
+                      </span>
+                    </p>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+
           </div>
           <div className="relative h-[calc(100vh-66px)] w-full">
             <div className="absolute bottom-[32px] w-full mx-auto left-0 right-0 flex justify-center items-end">
